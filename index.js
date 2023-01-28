b793dac235d2bad19e5c66e3b6ae1539876b22fd
Rachel Sokol <50994670+rmsokol@users.noreply.github.com>
2023-01-28 11:59:58 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+let dateEl = new Date()
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    countEl.textContent = 0
+    count = 0
+}
